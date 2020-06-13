d4a52676a9e89c2af7fe59ebf95b27a0f5ab9302
FrankSmith22 <fasvi6@gmail.com>
2020-06-12 20:41:43 -0400

    added routing for campsite details in directory;

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -20,8 +20,8 @@ function Contact(props) {
                     </address>
                 </div>
                 <div className="col">
-                    <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone"></i> 1-206-555-1234</a><br />
-                    <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o"></i> campsites@nucamp.co</a>
+                    <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
+                    <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o" /> campsites@nucamp.co</a>
                 </div>
             </div>
         </div>
